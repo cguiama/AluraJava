51d68964bd3418c57ea983ae0fbca643e03d1307
Guilherme <45200933+cguiama@users.noreply.github.com>
2022-01-27 23:07:03 -0300

    Boolean expressions introduction

---
+++ sintaxe/Main.java
@@ -0,0 +1,10 @@
+package sintaxe;
+
+public class Main {
+    public static void main(String[] args) {
+        int primeiro = 5;
+        int segundo = 7;
+
+        System.out.println();
+    }
+}
---
+++ sintaxe/TestaValores.java
@@ -0,0 +1,4 @@
+package sintaxe;
+
+public class TestaValores {
+}
